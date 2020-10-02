ab555b95d152e505b5c731e603d30b768d85cbf8
dev000077 <72245977+dev000077@users.noreply.github.com>
2020-10-02 12:10:12 +0530

    Create button.js
    PR1 added

---
+++ button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+
+const App = () => {
+   const handlePress = () => false
+   return (
+      <Button
+         onPress = {handlePress}
+         title = "Red button!"
+         color = "red"
+      />
+   )
+}
+export default App
